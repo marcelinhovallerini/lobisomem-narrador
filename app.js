db4a1f6fcf524d4e79aa2cf6562fe9e969be04af
marcelinhovallerini <marcelinhovallerini@yahoo.com.br>
2026-03-10 01:58:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -147,7 +147,7 @@ function resetGame(){
 
 async function startNight(){
 
-	await play(forest)
+	play(forest)
 
 	setInterval(() => {
   		owl.currentTime = 0;
@@ -392,6 +392,7 @@ async function startNight(){
 	await play(vote)
 
 }
+
 
 
 
