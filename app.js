c3a34d752fbdacbbe0c87a17a5af7d5a260e41a7
marcelinhovallerini <marcelinhovallerini@yahoo.com.br>
2026-03-12 12:17:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -99,6 +99,21 @@ vote:"narrator_vote.mp3"
 
 }
 
+function updateHeaderHeight(){
+
+const header = document.querySelector(".header")
+const height = header.offsetHeight
+
+document.documentElement.style.setProperty(
+"--header-height",
+height + "px"
+)
+
+}
+
+window.addEventListener("load", updateHeaderHeight)
+window.addEventListener("resize", updateHeaderHeight)
+
 const selectedRoles = {}
 
 function toggleRole(element, role, max){
